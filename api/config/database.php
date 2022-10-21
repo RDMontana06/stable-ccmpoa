72d0d9ba721cf0379a5101cfba3b611e7948fb8a
RDMontana06 <montanareymart06@gmail.com>
2022-10-21 19:00:40 +0800

    Remove Dummy

--- api/config/database.php
+++ api/config/database.php
@@ -1,7 +1,8 @@
 <?php
 // used to get mysql database connection
-class Database{
- 
+class Database
+{
+
     // specify your own database credentials
     // private $host = "localhost";
     // private $db_name = "dbk3dxay3mldbg";
@@ -13,19 +14,19 @@ class Database{
     private $username = "root";
     private $password = "";
     public $conn;
- 
+
     // get the database connection
-    public function getConnection(){
- 
+    public function getConnection()
+    {
+
         $this->conn = null;
- 
-        try{
+
+        try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-        }catch(PDOException $exception){
+        } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
- 
+
         return $this->conn;
     }
 }
-?>
